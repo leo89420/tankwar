b10486238f1980f9b5dd3c7852dc40b0e48f6e49
leo <leo89420@yahoo.com.tw>
2020-12-19 11:02:52 +0800

    第一次commit

---
+++ src/main/java/GameClient.java
@@ -0,0 +1,10 @@
+public class GameClient {
+    public static void main(String[] args) {
+        System.out.println("莊");
+        System.out.println("凱");
+        System.out.println("祺");
+        System.out.println("請");
+        System.out.println("吃");
+        System.out.println("飯");
+    }
+}
